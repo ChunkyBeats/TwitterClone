49b1a6a99f448589d067f26dcd26f60a9bef7631
Michael Yanchar & Cory Ribson <student@epicodus.com>
2015-06-09 15:42:39 -0700

    Added search by hash tag functionailty

--- router.js
+++ router.js
@@ -2,5 +2,5 @@ Twitter.Router.map(function() {
   this.resource('tweets', {path: '/'});
   this.resource('tweet', {path: 'tweets/:tweet_id'});
   this.resource('addTweet');
-
+  this.resource('hashtags');
 })
--- store.js
+++ store.js
@@ -4,34 +4,38 @@ var tweets = [
     text: "I like food",
     time: "12:35",
     date: "09-08-2015",
-    username: "Person1"
+    hashtags: ["#food"]
   },
   {
     id: 2,
     text: "I like mexican food",
     time: "12:39",
     date: "05-08-2015",
-    username: "Person1"
+    hashtags: ["#mexican","#food"]
   },
-  {
-    id: 3,
-    text: "I like thai food",
-    time: "12:10",
-    date: "07-09-2015",
-    username: "Person1"
-  },
-  {
-    id: 4,
-    text: "I like irish food",
-    time: "12:56",
-    date: "05-09-2015",
-    username: "Person1"
-  },
-  {
-    id: 5,
-    text: "I like mexican food",
-    time: "2:35",
-    date: "12-09-2015",
-    username: "Person2"
-  },
+  // {
+  //   id: 3,
+  //   text: "I like thai food",
+  //   time: "12:10",
+  //   date: "07-09-2015",
+  //   username: "Person1"
+  // },
+  // {
+  //   id: 4,
+  //   text: "I like irish food",
+  //   time: "12:56",
+  //   date: "05-09-2015",
+  //   username: "Person1"
+  // },
+  // {
+  //   id: 5,
+  //   text: "I like mexican food",
+  //   time: "2:35",
+  //   date: "12-09-2015",
+  //   username: "Person2"
+  // },
+];
+
+var matchTags = [
+
 ];
